59c03a7847f24c82905459e5006a6248abca5329
Oinari-Shinto <oinari.shinto@gmail.com>
2021-11-02 18:19:54 +0300

    create full header components

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,17 @@
-import {HomeIcon} from "@heroicons/react/outline";
+import {BadgeCheckIcon, HomeIcon, LightningBoltIcon, MapIcon, SearchIcon, UserIcon} from "@heroicons/react/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
     return (
-        <header className=''>
-            <div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
                 <HeaderItem title='HOME' Icon={HomeIcon}/>
+                <HeaderItem title='MODEL' Icon={LightningBoltIcon}/>
+                <HeaderItem title='О Нас' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+                <HeaderItem title='CONTACT' Icon={MapIcon}/>
+                <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
             </div>
             <Image
                 className="object-contain" 
